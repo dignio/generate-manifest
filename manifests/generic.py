ec60e9c326bbc40f92a6b332b68980de7f6dd3cf
Bjarne Oeverli <bjarne.oeverli@gmail.com>
2022-01-17 09:09:00 +0100

    A spread of the inputs as a dict will not work, pass it along as an object

--- manifests/generic.py
+++ manifests/generic.py
@@ -6,13 +6,12 @@ from manifests import get_manifest_template
 
 
 class GenericManifest(Chart):
-
     @classmethod
     def from_inputs(cls, scope: Construct, filename: str, inputs: Inputs):
         """Create a Manifest from input parameters."""
         # Get the appropriate template
         manifest = GenericManifest(scope, filename)
-        template = get_manifest_template(inputs.id)
+        template = get_manifest_template(inputs.app_name)
 
         # Instantiate the template
-        template.from_inputs(manifest, **inputs.as_dict())
+        template.from_inputs(manifest, inputs)
